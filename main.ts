2f0d2ea2097d6f53a8074ba998424a042b59edd6
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2023-04-20 17:43:19 -0400

    Add mini-menu extension

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@
  * Template last update: 17 Apr 2023 ak
  * 
  * Immediate TODO List
- * - [ ] Add mini menu extension. See https://forum.makecode.com/t/arcade-mini-menu-extension/14368
+ * - [X] Add mini menu extension. See https://forum.makecode.com/t/arcade-mini-menu-extension/14368
  * - [ ] Implement game load mechanism from attract menu.
  * - [ ] Add function in Option Screens plugin to add items to system menu.  See https://forum.makecode.com/t/custom-menu-showcase/18381
  * - [ ] Add game save item to system menu.
